e5a3379b5c68ac412d4a68df53d9063b30929669
kcaras <caraskimberly@yahoo.com>
2020-03-22 22:02:52 -0400

    added combined model with input text

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -307,6 +307,151 @@ def sample_combined_models(
             f.close()
     return all_txt
 
+def sample_combined_models_with_seed(
+    model_name='117M',
+    run_name1='kdrama_finetune',
+    run_name2='dnd_bios2',
+    seed=None,
+    nsamples=4,
+    batch_size=1,
+    length=200,
+    temperature=1,
+    top_k=40,
+    top_k_combined=0.0,
+    top_p=0.0,
+    weight1=0.4,
+    weight2=0.6,
+    use_random=False,
+    use_swap=False,
+    use_fifty_one=False,
+    use_vanilla=False,
+    debug=False,
+    raw_text=''
+):
+    """
+    Run the sample_model
+    :model_name=117M : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to
+     reproduce results
+    :nsamples=0 : Number of samples to return, if 0, continues to
+     generate samples indefinately.
+    :batch_size=1 : Number of batches (only affects speed/memory).
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
+    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
+     overriding top_k if set to a value > 0. A good setting is 0.9.
+    """
+    enc = encoder.get_encoder(model_name)
+    hparams = model.default_hparams()
+    with open(os.path.join('models', model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+        context = tf.placeholder(tf.int32, [batch_size, None])
+        output = sample.sample_sequence_combined(
+            hparams=hparams, run_name1=run_name1, run_name2=run_name2,
+            context=context,
+            length=length,
+            start_token=enc.encoder['<|endoftext|>'],
+            batch_size=batch_size,
+            temperature=temperature,
+            top_k=top_k,
+            top_k_combined=top_k_combined,
+            top_p=top_p,
+            weight1=weight1,
+            weight2=weight2,
+            use_random=use_random,
+            use_swap=use_swap,
+            use_fifty_one=use_fifty_one,
+            debug=debug,
+            use_vanilla=use_vanilla
+        )
+
+        saver1 = tf.train.Saver([v for v in tf.all_variables() if run_name1 in v.name])
+        ckpt1 = tf.train.latest_checkpoint(os.path.join('checkpoint', run_name1))
+        saver1.restore(sess, ckpt1)
+        saver2 = tf.train.Saver([v for v in tf.all_variables() if run_name2 in v.name])
+        ckpt2 = tf.train.latest_checkpoint(os.path.join('checkpoint', run_name2))
+        saver2.restore(sess, ckpt2)
+        ckpt3 = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        saver3 = tf.train.Saver([v for v in tf.all_variables() if 'model' in v.name])
+        saver3.restore(sess, ckpt3)
+        if top_k_combined > 0:
+            out_file = 'samples/{}_{}/two_pasts/static_weights/temp_{}_len_{}_p_{}_combined_k_{}_w1_{}_w2_{}.txt'.format(
+                run_name1, run_name2, temperature, str(length), top_p, top_k_combined, weight1, weight2)
+        elif use_vanilla:
+            out_file = 'samples/{}_{}/two_pasts/static_weights/vanilla_temp_{}_len_{}_p_{}_top_k_{}_w1_{}_w2_{}.txt'.format(
+                run_name1, run_name2, temperature, str(length), top_p, top_k_combined, weight1, weight2)
+        elif use_fifty_one:
+            out_file = 'samples/{}_{}/two_pasts/fifty_one_weights/rand_temp_{}_len_{}_p_{}_k_{}_w1_{}_w2_{}.txt'.format(run_name1, run_name2, temperature, str(length), top_p, top_k, weight1, weight2)
+        elif use_random:
+            out_file = 'samples/{}_{}/two_pasts/random_weights/rand_temp_{}_len_{}_p_{}_k_{}_w1_{}_w2_{}.txt'.format(run_name1, run_name2, temperature, str(length), top_p, top_k, weight1, weight2)
+        elif use_swap:
+            out_file = 'samples/{}_{}/two_pasts/swap_weights/swap_temp_{}_len_{}_p_{}_k_{}_w1_{}_w2_{}.txt'.format(run_name1,
+                                                                                                           run_name2,
+                                                                                                           temperature,
+                                                                                                           str(length),
+                                                                                                           top_p, top_k,
+                                                                                                           weight1,
+                                                                                                           weight2)
+        else:
+            out_file = 'samples/{}_{}/two_pasts/static_weights/temp_{}_len_{}_p_{}_k_{}_w1_{}_w2_{}.txt'.format(run_name1, run_name2,temperature, str(length), top_p, top_k, weight1, weight2)
+        f = open(out_file, 'w', encoding='utf-8')
+
+        cnt = 0
+        generated = 0
+        all_txt = ''
+        if False:
+            writer = tf.summary.FileWriter(
+                '/logs')
+            writer.add_graph(sess.graph)
+            out = sess.run(output)
+
+            cnt += 1
+            for i in range(batch_size):
+                generated += batch_size
+                #text = enc.decode(out[i])
+                #sample_str = '\n' + "=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40 + '\n'
+               # print(sample_str)
+               # print(text)
+               # f.write(sample_str)
+               # f.write(text)
+            f.close()
+        else:
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            all_text = ''
+            while nsamples == 0 or generated < nsamples:
+                out, mx = sess.run(output, feed_dict={context: [context_tokens for _ in range(batch_size)]})
+                out = out[:, len(context_tokens):]
+                cnt += 1
+                for i in range(batch_size):
+                    generated += batch_size
+                    text = enc.decode(out[i])
+                    sample_str = '\n' + "=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40 + '\n'
+                    print(sample_str)
+                    print(text)
+                    f.write(sample_str)
+                    f.write(text)
+                    all_txt += text
+            f.close()
+    return all_txt
+
 
 def print_logits(
     model_name='117M',
